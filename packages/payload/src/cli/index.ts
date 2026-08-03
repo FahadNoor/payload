afc377cb14bfe37ca4962e9ecb6730b658844580
Alessio Gravili <github@gravili.net>
2026-08-03 01:17:52 -0700

    data command

--- packages/payload/src/cli/index.ts
+++ packages/payload/src/cli/index.ts
@@ -12,7 +12,6 @@ import { parsePayloadComponent } from './commands/generateImportMap/utilities/pa
 import { loadEnv } from './loadEnv.js'
 
 configureZod(en())
-
 export const createProgram = async (args: CLIArgs): Promise<Command> => {
   const program = new Command()
     .name('payload')
